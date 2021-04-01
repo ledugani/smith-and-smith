c33d7f8073b65b14432b453cc150ee5289232e86
ledugani <tdugan85@gmail.com>
2021-04-01 01:05:33 -0500

    finally figured out how to write content to the page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,9 +1,25 @@
 @extends('layouts.app')
 
 @section('content')
-  @include('partials.page-header')
+  
 
-  @if (!have_posts())
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="jumbotron">
+          <h1 class="display-4">Hello, world!</h1>
+          <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+          <hr class="my-4">
+          <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+          <p class="lead">
+            <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- @if (!have_posts())
     <div class="alert alert-warning">
       {{ __('Sorry, no results were found.', 'sage') }}
     </div>
@@ -15,4 +31,5 @@
   @endwhile
 
   {!! get_the_posts_navigation() !!}
+  <div>HELP ME</div> -->
 @endsection
