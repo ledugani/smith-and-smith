b9e73f386ac6ae4781b97932d2ea3977bf5411c9
ledugani <tdugan85@gmail.com>
2021-04-01 14:59:01 -0500

    added hero section

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,13 +1,33 @@
 <header class="banner">
-  <div class="container">
-    <nav class="navbar navbar-expand-md navbar-light bg-light">
-      <img src="<?= get_template_directory_uri(); ?>/assets/images/s&s_logo.png" height="30" alt="">
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-primary" aria-controls="navbar-primary" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"><i class="fa fa-bars"></i></span>
-      </button>
-      @if (has_nav_menu('primary_navigation'))
-        {!! wp_nav_menu($primarymenu) !!}
-      @endif
-    </nav>
-  </div>
+  <nav class="navbar navbar-expand-md navbar-light bg-light">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-4">
+          <img 
+            src="<?= get_template_directory_uri(); ?>/assets/images/s&s_logo.png" 
+            height="30" 
+            alt=""
+          >
+          <button 
+            class="navbar-toggler" 
+            type="button" 
+            data-toggle="collapse" 
+            data-target="#navbar-primary" 
+            aria-controls="navbar-primary" 
+            aria-expanded="false" 
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon">
+              <i class="fa fa-bars"></i>
+            </span>
+          </button>
+        </div>
+        <div class="col-sm-8">
+          @if (has_nav_menu('primary_navigation'))
+            {!! wp_nav_menu($primarymenu) !!}
+          @endif
+        </div>
+      </div>
+    </div>
+  </nav>
 </header>
